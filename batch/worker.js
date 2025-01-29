dc8c50be7fa139c043e23551a8e8544cf6aa5d87
SHDPIXEL <shdpixel@gmail.com>
2025-01-29 15:29:00 +0530

    Update worker.js

--- batch/worker.js
+++ batch/worker.js
@@ -15,43 +15,43 @@ const redisConfig = {
   port: 6379, // Replace with your Redis port (default is 6379)
 };
 
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+// const s3Client = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   },
+// });
 
-async function downloadFromS3(s3Url, localPath) {
-  const bucketName = process.env.AWS_S3_BUCKET;
-  const key = s3Url.split(".com/")[1];
+// async function downloadFromS3(s3Url, localPath) {
+//   const bucketName = process.env.AWS_S3_BUCKET;
+//   const key = s3Url.split(".com/")[1];
 
-  const response = await s3Client.send(
-    new GetObjectCommand({
-      Bucket: bucketName,
-      Key: key,
-    })
-  );
+//   const response = await s3Client.send(
+//     new GetObjectCommand({
+//       Bucket: bucketName,
+//       Key: key,
+//     })
+//   );
 
-  return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(localPath);
-    response.Body.pipe(writeStream)
-      .on("finish", () => resolve())
-      .on("error", reject);
-  });
-}
+//   return new Promise((resolve, reject) => {
+//     const writeStream = fs.createWriteStream(localPath);
+//     response.Body.pipe(writeStream)
+//       .on("finish", () => resolve())
+//       .on("error", reject);
+//   });
+// }
 
 // Create a Worker
 const videoWorker = new Worker(
   "videoProcessing",
   async (job) => {
     const { videoId, videoS3Url, templateS3Url, text } = job.data;
-    const tempDir = path.join(os.tmpdir(), uuidv4());
-    fs.mkdirSync(tempDir, { recursive: true });
+    // const tempDir = path.join(os.tmpdir(), uuidv4());
+    // fs.mkdirSync(tempDir, { recursive: true });
 
-    const tempVideoPath = path.join(tempDir, "input.mp4");
-    const tempTemplatePath = path.join(tempDir, "template.png");
+    // const tempVideoPath = path.join(tempDir, "input.mp4");
+    // const tempTemplatePath = path.join(tempDir, "template.png");
     console.log("Raw data video:",job.data)
     // Debug logging
     console.log("Processed video:", {
@@ -73,21 +73,21 @@ const videoWorker = new Worker(
       await form.save();
 
       // Download files from S3
-      await downloadFromS3(videoS3Url, tempVideoPath);
-      await downloadFromS3(templateS3Url, tempTemplatePath);
+      // await downloadFromS3(videoS3Url, tempVideoPath);
+      // await downloadFromS3(templateS3Url, tempTemplatePath);
 
       // Debug logging
       console.log("Processing video:", {
-        tempVideoPath,
-        tempTemplatePath,
+        videoS3Url,
+        templateS3Url,
         text,
         videoId,
       });
 
       // Process the video
       const processedVideoS3Url = await processVideo(
-        tempVideoPath,
-        tempTemplatePath,
+        videoS3Url,
+        templateS3Url,
         text,
         videoId
       );
