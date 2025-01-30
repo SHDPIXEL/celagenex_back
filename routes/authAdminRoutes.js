d7645897f1592779b927dac42ce34550fad3f464
SHDPIXEL <shdpixel@gmail.com>
2025-01-31 03:20:54 +0530

    Update authAdminRoutes.js

--- routes/authAdminRoutes.js
+++ routes/authAdminRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { login,getDashboardStats, verifyAdminToken,createUserA, downloadData } = require('../controller/authControlleradmin');
+const { login,getDashboardStats, verifyAdminToken,createUserA, downloadData, getPendingVideos } = require('../controller/authControlleradmin');
 
 router.post('/login', login);
 router.get('/dashboard-stats',verifyAdminToken, getDashboardStats);
 router.post('/create-user',verifyAdminToken, createUserA);
 router.get('/download-data',verifyAdminToken, downloadData);
+router.get('/process-pending-videos',verifyAdminToken, getPendingVideos);
 
 module.exports = router;
