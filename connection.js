6320a87ea42d5630dd91389f984c05224e36b5d7
SHDPIXEL <shdpixel@gmail.com>
2025-01-31 00:55:10 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('celagenex','celagenex_u','8u3Hs4RBiaATxj%xfRV$KT_cG',{
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql'
 });
 
