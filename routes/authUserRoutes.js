c306a5f2a7bf76df6bcf94c4962c6fe2f272364c
SHDPIXEL <shdpixel@gmail.com>
2025-02-06 17:01:28 +0530

    Update authUserRoutes.js

--- routes/authUserRoutes.js
+++ routes/authUserRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { login,verifyUserToken,formSubmit } = require('../controller/authControlleruser');
-const { getFormDataByUserId, getAllUsersData } = require('../controller/userController');
+const { getFormDataByUserId, getAllUsersData,searchUsersData } = require('../controller/userController');
 
 router.post('/login', login);
 router.post('/formsubmit',verifyUserToken, formSubmit);
 router.get('/getAllVideos',verifyUserToken, getAllUsersData);
 router.get('/getFormDataByUserId/:userId',verifyUserToken, getFormDataByUserId);
+router.get('/searchVideos',verifyUserToken, searchUsersData);
 
 module.exports = router;
