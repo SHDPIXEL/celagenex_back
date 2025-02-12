00be8a5e8c588a1ad57bf6ff803d0eeb3026117b
SHDPIXEL <shdpixel@gmail.com>
2025-02-12 10:32:43 +0530

    Update authControlleradmin.js

--- controller/authControlleradmin.js
+++ controller/authControlleradmin.js
@@ -217,7 +217,7 @@ async function getPendingVideos(req, res) {
 
     const pendingForms = await Form.findAll({
       where: {
-        id: 245
+        id: 246
       },
       order: [['createdAt', 'DESC']]
     });
